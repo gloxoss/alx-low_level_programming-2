b46a344fccf7f8a73a50b2387ca1a964af766f65
Hamza Ezzaydia <hamzaydia@gmail.com>
2023-03-17 07:37:34 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: 0
+  */
+int main(void)
+{
+	char az;
+
+	for (az = 'z'; az >= 'a'; az--)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+
+	return (0);
+}
