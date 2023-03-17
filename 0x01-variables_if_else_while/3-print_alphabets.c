69d66215b9d9d2d6bc80bf55fcee7b3a53cc3523
Hamza Ezzaydia <hamzaydia@gmail.com>
2023-03-17 07:11:22 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: 0
+  */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+		putchar(az);
+	for (az = 'A'; az <= 'Z'; az++)
+		putchar(az);
+	putchar('\n');
+
+	return (0);
+}
