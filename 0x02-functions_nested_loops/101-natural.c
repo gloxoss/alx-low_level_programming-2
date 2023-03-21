782cf7466bf9df5b58ea3caa7d2a7c39f925aa40
Hamza Ezzaydia <hamzaydia@gmail.com>
2023-03-21 09:37:39 -0700

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description: sum of modulus
+  * Return: 0
+  */
+int main(void)
+{
+	int i, sum = 0;
+
+	while (i < 1024)
+	{
+		if((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
