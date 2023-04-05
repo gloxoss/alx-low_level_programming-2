ccc45a7aa991550b8892eaa656992bea39cce961
Hamza Ezzaydia <hamzaydia@gmail.com>
2023-04-04 23:07:39 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef _main_h_
+#define _main_h_
+
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _strlen_recursion(char *s);
+int _sqrt_recursion(int n);
+int is_palindrome(char *s);
+int _sqrt_subfunction(int n, int y);
+void _print_rev_recursion(char *s);
+int helper_prime(int num, int i);
+int is_prime_number(int n);
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
