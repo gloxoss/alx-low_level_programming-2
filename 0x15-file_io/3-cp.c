b2227ae718d1a54adcece217bed4d7caf517f343
Hamza Ezzaydia <hamzaydia@gmail.com>
2023-05-08 17:40:39 -0700

    possible cp fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp from to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 	from = open(argv[1], O_RDONLY);
