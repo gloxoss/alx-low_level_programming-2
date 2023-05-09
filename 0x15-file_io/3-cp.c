b55e61f582d100f87a62697dd9cdabb8ea4cc746
Hamza Ezzaydia <hamzaydia@gmail.com>
2023-05-08 17:22:06 -0700

    3. cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+  * main - Enry point
+  * @argc: int
+  * @argv: char*
+  * Return: int
+  */
+int main(int argc, char *argv[])
+{
+	int from, to, closer;
+	ssize_t nchars, nwr;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp from to\n");
+		exit(97);
+	}
+	from = open(argv[1], O_RDONLY);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	error_file(from, to, argv);
+	while ((nchars = read(from, buffer, 1024)) > 0)
+	{
+		nwr = write(to, buffer, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
+	closer = close(from);
+	if (closer == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		exit(100);
+	}
+	closer = close(to);
+	if (closer == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
+		exit(100);
+	}
+	return (0);
+}
+
+/**
+  * error_file - print file error message
+  * @from: int
+  * @to: int
+  * @argv: char*
+  * Return: void
+  */
+void error_file(int from, int to, char *argv[])
+{
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,6 @@ int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
+void error_file(int from, int to, char *argv[]);
 
 #endif
