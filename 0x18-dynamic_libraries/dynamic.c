950b431b9f2e3e45088cf334526a0855caf36e0c
Hamza Ezzaydia <hamzaydia@gmail.com>
2023-06-19 19:00:17 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/dynamic.c
@@ -0,0 +1,122 @@
+#include "main.h"
+#include <stddef.h>
+
+int _abs(int n) {
+    // Add the implementation of the _abs function here
+    // ...
+    return 0;
+}
+
+int _atoi(char *s) {
+    // Add the implementation of the _atoi function here
+    // ...
+    return 0;
+}
+
+int _isalpha(int c) {
+    // Add the implementation of the _isalpha function here
+    // ...
+    return 0;
+}
+
+int _isdigit(int c) {
+    // Add the implementation of the _isdigit function here
+    // ...
+    return 0;
+}
+
+int _islower(int c) {
+    // Add the implementation of the _islower function here
+    // ...
+    return 0;
+}
+
+int _isupper(int c) {
+    // Add the implementation of the _isupper function here
+    // ...
+    return 0;
+}
+
+int _putchar(char c) {
+    // Add the implementation of the _putchar function here
+    // ...
+    return 0;
+}
+
+void _puts(char *s) {
+    // Add the implementation of the _puts function here
+    // ...
+}
+
+int _strcmp(char *s1, char *s2) {
+    // Add the implementation of the _strcmp function here
+    // ...
+    return 0;
+}
+
+char *_strcpy(char *dest, char *src) {
+    // Add the implementation of the _strcpy function here
+    // ...
+    return dest;
+}
+
+int _strlen(char *s) {
+    // Add the implementation of the _strlen function here
+    // ...
+    return 0;
+}
+
+char *_strcat(char *dest, char *src) {
+    // Add the implementation of the _strcat function here
+    // ...
+    return dest;
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    // Add the implementation of the _strncat function here
+    // ...
+    return dest;
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    // Add the implementation of the _strncpy function here
+    // ...
+    return dest;
+}
+
+char *_strchr(char *s, char c) {
+    // Add the implementation of the _strchr function here
+    // ...
+    return NULL;
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    // Add the implementation of the _strspn function here
+    // ...
+    return 0;
+}
+
+char *_strpbrk(char *s, char *accept) {
+    // Add the implementation of the _strpbrk function here
+    // ...
+    return NULL;
+}
+
+char *_strstr(char *haystack, char *needle) {
+    // Add the implementation of the _strstr function here
+    // ...
+    return NULL;
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    // Add the implementation of the _memset function here
+    // ...
+    return s;
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    // Add the implementation of the _memcpy function here
+    // ...
+    return dest;
+}
+
